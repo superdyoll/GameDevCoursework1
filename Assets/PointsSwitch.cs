de3339a7dddb789711b82483b0d5e01223b627f2
Edmund <User@10.9.129.71>
2017-02-12 22:51:01 +0000

    Implemented a points based system based on clicks detected via collision meshes, this will be replaced soon with ray casting from the camera.

---
+++ Assets/PointsSwitch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointsSwitch : MonoBehaviour {
+
+    public GameObject[] tracks;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        foreach (GameObject track in tracks)
+        {
+            track.SetActive(!track.activeSelf);
+        }
+    }
+}
