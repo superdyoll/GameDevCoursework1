c648eb55b333c4d440750da31e6b1ca2d0351f95
Edmund <ek7g14@ston.ac.uk>
2017-03-02 16:17:58 +0000

    implemented a method for choosing win colours

--- Assets/TruckGeneration.cs
+++ Assets/TruckGeneration.cs
@@ -4,22 +4,15 @@ using UnityEngine;
 
 public class TruckGeneration : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-
     // Update is called once per frame
     public float intervalMin = 5f;
     public float intervalMax = 15f;
     public Material[] materials;
     public GameObject prefab;
-    public Vector3 startSpawnLoc;
-    public Vector3 endSpawnLoc;
+    public Vector3 spawnLoc;
 
     private float timeNow = 0f; 
     private float timeNext = 0f;
-    private Vector3 direction;
 
 	void FixedUpdate ()
     {
@@ -28,23 +21,14 @@ public class TruckGeneration : MonoBehaviour {
 		//check time
         if (timeNext < timeNow)
         {
-            //    Debug.Log("Time passed");
-            //check space - fixed space
-            direction = endSpawnLoc - startSpawnLoc;
-            Debug.Log(direction);
-            for (int i = 0; i <= direction.magnitude; i++)
+            if (Physics.OverlapBox(spawnLoc, new Vector3(5f,0f, 5f)).Length == 0)
             {
-                if (Physics.OverlapBox(startSpawnLoc + i * direction.normalized, new Vector3(5f,0f, 5f)).Length == 0)
-                {
-                    Debug.Log(i);
-                    //create a new object with random colour
-                    GameObject newTruck = Instantiate(prefab, startSpawnLoc + i * direction.normalized, Quaternion.identity);
-                    Renderer rend = newTruck.GetComponent<Renderer>();
-                    rend.material = materials[Random.Range(0, materials.Length - 1)];
-                    //set new timeNext for the next spawn
-                    timeNext = timeNow + Random.Range(intervalMin, intervalMax);
-                    break;
-                }
+                //create a new object with random colour
+                GameObject newTruck = Instantiate(prefab, spawnLoc, Quaternion.identity);
+                Renderer rend = newTruck.GetComponent<Renderer>();
+                rend.material = materials[Random.Range(0, materials.Length - 1)];
+                //set new timeNext for the next spawn
+                timeNext = timeNow + Random.Range(intervalMin, intervalMax);
             }
         }
     }
--- Assets/WinDetection.cs
+++ Assets/WinDetection.cs
@@ -6,6 +6,43 @@ using UnityEngine.SceneManagement;
 public class WinDetection : MonoBehaviour {
 
     public string levelToLoad;
+    public Canvas canvas;
+    public int winByTruckX;
+    public int sizeWin;
+
+    private List<GameObject> winTruckList;
+    private GameObject[] allTrucks;
+
+    void Start()
+    {
+        //genWinTruckList();
+
+    }
+
+    private void genWinTruckList()
+    {
+        List<int> usedTrucks = new List<int>();
+        int tempRandom = 0;
+        bool duplicate;
+        for (int i = 0; i < sizeWin; i++)
+        {
+            duplicate = true;
+            while (duplicate)
+            {
+                tempRandom = Random.Range(0, winByTruckX);
+                if (!usedTrucks.Contains(tempRandom))
+                {
+                    usedTrucks.Add(tempRandom);
+                    duplicate = false;
+                }
+            }
+            usedTrucks.Add(tempRandom);
+        }
+        foreach (int i in usedTrucks)
+        {
+            winTruckList.Add(allTrucks[i]);
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
