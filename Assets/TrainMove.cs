8ae16a2e34dd68b654bb198b9dc88eb5db1fbdb4
superdyoll <superdyoll@gmail.com>
2017-03-03 13:21:29 +0000

    Train Coupling

--- Assets/TrainMove.cs
+++ Assets/TrainMove.cs
@@ -1,26 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrainMove : MonoBehaviour {
+public class TrainMove : MonoBehaviour, ICube
+{
 
-    //public GameObject cube;
+    public GameObject train;
     //private Rigidbody rb;
-
 
     public float smooth = 3;
     public int layerMask = (1 << 8);
+    private float direction;
+
+    public GameObject[] linked;
 
     // Use this for initialization
-    void Start () {
-		//rb = cube.GetComponent<Rigidbody>();
-	}
+    void Start()
+    {
+        linked = new GameObject[2];
+        linked[0] = null;
+        linked[1] = null;
+        //rb = cube.GetComponent<Rigidbody>();
+    }
 
-    private void FixedUpdate()
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("SPAAAAACCEE");
+            foreach (GameObject link in linked)
+            {
+                if (link != null)
+                {
+                    TruckMove truckMove = link.GetComponent<TruckMove>();
+                    if (truckMove != null)
+                    {
+                        truckMove.RemoveAllConnections();
+                    }
+                }
+            }
+        }
+    }
+
+    void FixedUpdate()
     {
         // Move left/right with <- and -> or 'a' and 'd'
+        direction = Input.GetAxis("Horizontal");
 
-        float direction = Input.GetAxis("Horizontal");
         //Debug.Log(direction);
         if (direction != 0)
         {
@@ -29,20 +56,36 @@ public class TrainMove : MonoBehaviour {
             for (float i = -smooth; i <= smooth; i++)
             {
                 Vector3 rayPosition = horizonalPosition + new Vector3(0, 0, i);
+
                 Ray rayMove = new Ray(rayPosition, Vector3.down);
                 RaycastHit hitInfoMove, hitInfoTruck;
                 if (Physics.Raycast(rayMove, out hitInfoMove, 25f, layerMask))
                 {
-                    if (Physics.Raycast(rayMove, out hitInfoTruck, 25f))
+                    Vector3 angle = rayPosition - transform.position;
+                    angle.Normalize();
+
+                    Ray rayTruck = new Ray(rayPosition, angle);
+                    if (Physics.Raycast(rayTruck, out hitInfoTruck, 7f))
                     {
-                        if (hitInfoTruck.collider.gameObject.tag != "Truck")
+                        if (hitInfoTruck.collider.gameObject.tag == "Truck")
                         {
-                            transform.position = rayPosition;
-                            break;
-                        }else
-                        {
-                            print("TRUCK!!!!");
+                            GameObject truck = hitInfoTruck.collider.gameObject;
+                            TruckMove truckmove = truck.GetComponent<TruckMove>();
+                            if (direction < 0)
+                            {
+                                linked[0] = truck;
+                            }
+                            else
+                            {
+                                linked[1] = truck;
+                            }
+                            truckmove.AddConnection(train, direction);
                         }
+                    }
+                    else
+                    {
+                        transform.position = rayPosition;
+                        break;
                     }
                 }
             }
@@ -69,5 +112,20 @@ public class TrainMove : MonoBehaviour {
                 transform.rotation = newRotation;
             }
         }
+    }
+
+    public float GetDirection()
+    {
+        return direction;
+    }
+
+    public GameObject GetTruckOnRight()
+    {
+        return linked[1];
+    }
+
+    public GameObject GetTruckOnLeft()
+    {
+        return linked[0];
     }
 }
--- Assets/TruckMove.cs
+++ Assets/TruckMove.cs
@@ -1,75 +1,209 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TruckMove : MonoBehaviour
+public class TruckMove : MonoBehaviour, ICube
 {
-    public GameObject cube;
+    public GameObject truck;
     private Rigidbody rb;
 
 
     public float smooth = 3;
     public int layerMask = (1 << 8);
 
+    private float direction;
+
+    private GameObject[] linked;
+    private TrainMove train;
+
     // Use this for initialization
     void Start()
     {
-        rb = cube.GetComponent<Rigidbody>();
+        rb = truck.GetComponent<Rigidbody>();
+        linked = new GameObject[2];
+        RemoveAllConnections();
     }
 
     private void FixedUpdate()
     {
-        Quaternion rotation = transform.rotation;
-        Ray rayJoin = new Ray(transform.position, Vector3.left);
-
         //vert is w/s
         //horiz is a/d
-        if (rb.velocity != Vector3.zero)
+        if (linked[0] != null || linked[1] != null)
         {
-            Vector3 horizonalPosition = transform.position + new Vector3(rb.velocity.x * smooth, 0);
-            for (float i = -smooth; i <= smooth; i++)
+            direction = train.GetDirection();
+            if (direction != 0)
             {
-                Vector3 rayPosition = horizonalPosition + new Vector3(0, 0, i);
-                Ray rayMove = new Ray(rayPosition, Vector3.down);
-                RaycastHit hitInfoMove;
-                if (Physics.Raycast(rayMove, out hitInfoMove, 25f, layerMask))
+                Vector3 horizonalPosition = transform.position + new Vector3(direction * smooth, 0);
+                for (float i = -smooth; i <= smooth; i++)
                 {
-                    transform.position = rayPosition;
-                    break;
+                    Vector3 rayPosition = horizonalPosition + new Vector3(0, 0, i);
+                    Ray rayMove = new Ray(rayPosition, Vector3.down);
+                    RaycastHit hitInfoMove, hitInfoTruck;
+                    if (Physics.Raycast(rayMove, out hitInfoMove, 25f, layerMask))
+                    {
+                        Vector3 angle = rayPosition - transform.position;
+                        angle.Normalize();
+
+                        Ray rayTruck = new Ray(rayPosition, angle);
+                        if (Physics.Raycast(rayTruck, out hitInfoTruck, 7f))
+                        {
+                            if (hitInfoTruck.collider.gameObject.tag == "Truck")
+                            {
+                                TruckMove truckmove = hitInfoTruck.collider.gameObject.GetComponent<TruckMove>();
+                                truckmove.AddConnection(truck, direction);
+                            }
+                        }
+                        else
+                        {
+                            transform.position = rayPosition;
+                            break;
+                        }
+                    }
                 }
-            }
 
-            // Rotation
-            Ray ray = new Ray(transform.position, Vector3.down);
-            RaycastHit hitInfoRotate;
+                // Rotation
+                Ray ray = new Ray(transform.position, Vector3.down);
+                RaycastHit hitInfoRotate;
 
-            if (Physics.Raycast(ray, out hitInfoRotate, 100f, layerMask))
-            {
-                //Quaternion newRotation = Quaternion.LookRotation(new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")), hitInfo.transform.forward);
-                //newRotation *= Quaternion.Euler(0, 90, 0);
-                Quaternion newRotation = Quaternion.LookRotation(hitInfoRotate.transform.right, hitInfoRotate.transform.up);
-                float yComponent = newRotation.eulerAngles.y;
-                if (Mathf.Abs(yComponent - transform.rotation.eulerAngles.y) != 0)
+                if (Physics.Raycast(ray, out hitInfoRotate, 100f, layerMask))
                 {
+                    //Quaternion newRotation = Quaternion.LookRotation(new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")), hitInfo.transform.forward);
+                    //newRotation *= Quaternion.Euler(0, 90, 0);
+                    Quaternion newRotation = Quaternion.LookRotation(hitInfoRotate.transform.right, hitInfoRotate.transform.up);
+                    float yComponent = newRotation.eulerAngles.y;
+                    if (Mathf.Abs(yComponent - transform.rotation.eulerAngles.y) != 0)
+                    {
 
-                    //Debug.Log(Mathf.Abs(yComponent - transform.rotation.eulerAngles.y) + " , ycomp: " + yComponent + " , old ycomp: " + transform.eulerAngles.y);
+                        //Debug.Log(Mathf.Abs(yComponent - transform.rotation.eulerAngles.y) + " , ycomp: " + yComponent + " , old ycomp: " + transform.eulerAngles.y);
 
+                    }
+                    if (Mathf.Abs(yComponent - transform.rotation.eulerAngles.y) >= 180)
+                    {
+                        float temp = yComponent - 180;
+                        //Debug.Log("Old y: " + transform.rotation.eulerAngles.y + ", Newer y: " + yComponent + "Math1: " + Mathf.Abs(yComponent - transform.rotation.eulerAngles.y) + "Math2:" + temp);
+                        yComponent = yComponent - 180;
+                    }
+                    //this if statement may be unecessary
+                    if (yComponent >= 360)
+                    {
+                        yComponent = yComponent - 360;
+                    }
+                    newRotation = Quaternion.Euler(newRotation.eulerAngles.x, yComponent, newRotation.eulerAngles.z);
+                    transform.rotation = newRotation;
                 }
-                if (Mathf.Abs(yComponent - transform.rotation.eulerAngles.y) >= 180)
-                {
-                    float temp = yComponent - 180;
-                    //Debug.Log("Old y: " + transform.rotation.eulerAngles.y + ", Newer y: " + yComponent + "Math1: " + Mathf.Abs(yComponent - transform.rotation.eulerAngles.y) + "Math2:" + temp);
-                    yComponent = yComponent - 180;
-                }
-                //this if statement may be unecessary
-                if (yComponent >= 360)
-                {
-                    yComponent = yComponent - 360;
-                }
-                newRotation = Quaternion.Euler(newRotation.eulerAngles.x, yComponent, newRotation.eulerAngles.z);
-                transform.rotation = newRotation;
             }
         }
+    }
 
+    private bool AddConnection(GameObject link, int position)
+    {
+        if (linked[position] == null)
+        {
+            linked[position] = link;
+        }
+        else
+        {
+            return false;
+        }
+        AddTrain(link);
+        return true;
+    }
+
+    public bool AddConnection(GameObject link, float direction)
+    {
+        if (direction > 0)
+        {
+            return AddConnectionLeft(link);
+        }
+        else
+        {
+            return AddConnectionRight(link);
+        }
+    }
+
+    private bool AddTrain(GameObject link)
+    {
+        if (link.tag == "Train")
+        {
+            train = link.GetComponent<TrainMove>();
+        }
+        else
+        {
+            train = link.GetComponent<TruckMove>().GetTrain();
+        }
+        return true;
+    }
+
+    public bool AddConnectionLeft(GameObject leftLink)
+    {
+        print("Adding truck connection on left");
+        return AddConnection(leftLink, 0);
+    }
+
+    public bool AddConnectionRight(GameObject rightLink)
+    {
+        print("Adding truck connection on right");
+        return AddConnection(rightLink, 1);
+    }
+
+    public GameObject[] GetConnection()
+    {
+        print("Current connection is " + linked.ToString());
+        return linked;
+    }
+
+    public bool RemoveConnection(GameObject link)
+    {
+        bool returnBool = false;
+        if (linked[0] == link)
+        {
+            linked[0] = null;
+            returnBool = true;
+        }
+        else if (linked[1] == link)
+        {
+            linked[1] = null;
+            returnBool = true;
+        }
+        return returnBool;
+    }
+
+    public void RemoveAllConnections()
+    {
+        GameObject emptyGameObject = null;
+        RemoveAllConnections(emptyGameObject);
+    }
+
+    public void RemoveAllConnections(GameObject caller)
+    {
+        foreach (GameObject link in linked)
+        {
+            if (link != null)
+            {
+                TruckMove localTruck = link.GetComponent<TruckMove>();
+                if (localTruck != null && localTruck != caller)
+                {
+                    localTruck.RemoveAllConnections(truck);
+                }
+            }
+        }
+        linked[0] = null;
+        linked[1] = null;
+    }
+
+    public TrainMove GetTrain()
+    {
+        return train;
+    }
+
+    public GameObject GetTruckOnRight()
+    {
+        return linked[1];
+    }
+
+    public GameObject GetTruckOnLeft()
+    {
+        return linked[0];
     }
 }
